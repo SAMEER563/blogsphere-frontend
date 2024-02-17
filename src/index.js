2df3c0fc5fedfab0c4e8702b6c4215a07e8b931b
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-02-17 22:46:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './pages/components/Layout';
+import ErrorPage from './pages/ErrorPage'
+import Home from './pages/Home'
+import PostDetail from './pages/PostDetail'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import UserProfile from './pages/UserProfile'
+import Authors from './pages/Authors'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import CategoryPosts from './pages/CategoryPosts'
+import AuthorPosts from './pages/AuthorPosts'
+import Dashboard from './pages/Dashboard'
+import Logout from './pages/Logout'
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+   path: "/",
+   element: <Layout />,
+   errorElement: <ErrorPage />,
+   children: [
+    {index: true, element: <Home />},
+    {path: "posts/:id", element: <PostDetail />},
+    {path: "register", element: <Register />},
+    {path: "login", element: <Login />},
+    {path: "profile/:id", element: <UserProfile />},
+    {path: "authors", element: <Authors />},
+    {path: "create", element: <CreatePost />},
+    {path: "posts/:id/edit", element: <EditPost />},
+    {path: "posts/categories/:category", element: <CategoryPosts />},
+    {path: "posts/users/:id", element: <AuthorPosts />},
+    {path: "myposts/:id", element: <Dashboard />},
+    {path: "logout", element: <Logout />},
+   ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
