bb320280de5520a2cae5f8b92e6ae57be04a1cc6
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-02-20 12:44:40 +0530

    register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [userData, setuserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-export default Register
+  const changeInputHandler = (e) => {
+    setuserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    <section className="register">
+      <div className="container">
+        <h2>Sign Up</h2>
+        <form className="form register__form">
+          <p className="form__error-message">This is an error message</p>
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            value={userData.name}
+            onChange={changeInputHandler}
+            autoFocus
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+           <input
+            type="password"
+            placeholder="Confirm password"
+            name="password2"
+            value={userData.password2}
+            onChange={changeInputHandler}
+          />
+          <button type="submit" className='btn primary'>Register</button>
+        </form>
+        <small>Already have an account? <Link to="/login">Sign in</Link></small>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
