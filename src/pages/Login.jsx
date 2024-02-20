0417665b44c6b0ebd95485e17e8c71521ff4db3b
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-02-20 12:44:18 +0530

    Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [userData, setuserData] = useState({
+  
+    email: "",
+    password: "",
+   
+  });
 
-export default Login
+  const changeInputHandler = (e) => {
+    setuserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    <section className="login">
+      <div className="container">
+        <h2>Sign In</h2>
+        <form className="form login__form">
+          <p className="form__error-message">This is an error message</p>
+          
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          
+          <button type="submit" className='btn primary'>Login</button>
+        </form>
+        <small>Don't have an account? <Link to="/register">Sign up</Link></small>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
